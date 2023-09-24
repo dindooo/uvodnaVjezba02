2e4e415e545c5b9986bd667c1ce40733217900ba
dinooo <145918377+dindooo@users.noreply.github.com>
2023-09-24 17:27:19 +0200

    Add files via upload

---
+++ uvodnaVjezba02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uvodnaVjezba02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            double c, d, rezultat;
+
+            a=10;
+            b=7;
+            c=12.5;
+            d=16.7;
+            rezultat=(a+b+c+d)/4;
+
+
+            Console.WriteLine("Prosječna vrijednost varijabli a, b, c i d iznosi : " + rezultat);
+            Console.ReadKey();
+        }
+    }
+}
